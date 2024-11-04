39d2d82b62eaaed9a81979a57ba26c6b80b04caa
Balaji <95331603+BalajiSwami@users.noreply.github.com>
2024-11-04 20:39:03 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -8,9 +8,13 @@ import { Router } from './src/Controller/router';
 let app = express();
 app.use(cors());
 app.use(express.json());
-dbConnection.getConnection()
+dbConnection.getConnection();
 
-app.use('/api/nodes', Router)
+app.use('/api/nodes', Router);
+
+app.get('/home', (req, res) => {
+    res.status(200).json('Welcome, your app is working well');
+});
 
 let PORT = process.env.PORT
 
